d082094d0940f76629196b05ba555791987a93ee
Adrian <adrzanbar@gmail.com>
2026-04-08 20:43:07 -0300

    Add pdf support for letters

--- src/tasks/send-due-letters.ts
+++ src/tasks/send-due-letters.ts
@@ -91,7 +91,7 @@ export const SendDueLetters: TaskConfig<'SendDueLetters'> = {
   slug: 'SendDueLetters',
   schedule: [
     {
-      cron: process.env.SEND_DUE_LETTERS_CRON || '0 8 * * *',
+      cron: '* * * * *',
       queue: 'letters',
     },
   ],
